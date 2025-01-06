0ac9f1952de20d1d52fdce5806612d94e21d875d
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-01-06 18:39:58 +0300

    update the multiplication interface

--- src/main/java/com/example/springboot/multiplication/service/MultiplicationService.java
+++ src/main/java/com/example/springboot/multiplication/service/MultiplicationService.java
@@ -1,5 +1,13 @@
 package com.example.springboot.multiplication.service;
 
-public interface MultiplicationService {
+import com.example.springboot.multiplication.domain.Multiplication;
 
+public interface MultiplicationService {
+/**\
+ * Create a multiplication object with 2 randomly generated number between 0 and 100
+ * 
+ * @return a multiplication object with 2 random numbers
+ */
+	
+	Multiplication createRandomMultiplication();
 }
