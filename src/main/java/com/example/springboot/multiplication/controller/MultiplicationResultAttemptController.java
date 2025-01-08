95bd3e3c9141fea5fd0a74ca9e37a1047191f0b3
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-01-09 00:49:32 +0300

    add the MultiplicationreesultAttemptController skeleton code

---
+++ src/main/java/com/example/springboot/multiplication/controller/MultiplicationResultAttemptController.java
@@ -0,0 +1,31 @@
+package com.example.springboot.multiplication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.multiplication.service.MultiplicationService;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/results")
+public final class MultiplicationResultAttemptController {
+	
+	private final MultiplicationService multiplicationService;
+	
+	@Autowired
+	public MultiplicationResultAttemptController(final MultiplicationService multiplicationService) {
+		this.multiplicationService = multiplicationService;
+	}
+	
+	@RequiredArgsConstructor
+	@NoArgsConstructor(force = true)
+	@Getter
+	public static final class ResultResponse{
+		private final boolean correct;
+	}
+
+}
---
+++ src/test/java/com/example/springboot/multiplication/controller/MultiplicationResultAttemptControllerTest.java
@@ -0,0 +1,5 @@
+package com.example.springboot.multiplication.controller;
+
+public class MultiplicationResultAttemptControllerTest {
+
+}
