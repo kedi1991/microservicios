95073d582d1da0803cd97c80650f89a1a416fcac
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-01-10 16:17:26 +0300

    complete the html page and js to process statistics on the calculations

--- src/main/resources/static/multiplication-client.js
+++ src/main/resources/static/multiplication-client.js
@@ -9,6 +9,31 @@ function updateMultiplication() {
 		$('.multiplication-b').empty().append(data.argB);
 	});
 }
+
+function updateStats(alias) {
+	$.ajax({url: "http://localhost:8080/results?alias=" + alias,}).then(
+			function(data) {
+				$('#stats-body').empty();
+				data.forEach(function(row) {
+								$('#stats-body').append('<tr><td>' + 
+													row.id + 
+													'</td>' + 
+													'<td>' + 
+													row.multiplication.factorA + 
+													' x ' + 
+													row.multiplication.factorB + 
+													'</td>' + 
+													'<td>' + 
+													row.resultAttempt + 
+													'</td>' + 
+													'<td>' + 
+													(row.correct === true ? 'YES' : 'NO') + 
+													'</td></tr>');
+						});	
+			});
+}
+
+
 $(document).ready(function() {
 	updateMultiplication();
 	$("#attempt-form").submit(function(event) {
